a81193d0c9c19783e70fe0378a3d04e851ea2d2b
VKalaitanov <kalaytanov93@gmail.com>
2024-10-24 11:08:14 -0700

    del data_collector

--- users/signals.py
+++ users/signals.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import Group, Permission
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
-
+from django.contrib.auth.models import Permission
+print(Permission.objects.all().values_list('codename', flat=True))
 
 @receiver(post_migrate)
 def create_managers_group(sender, **kwargs):
