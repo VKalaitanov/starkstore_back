cc14b66e02149a58e3dde5c427c92e586719fa07
VKalaitanov <kalaytanov93@gmail.com>
2024-10-24 11:42:48 -0700

    change settings

--- starkstore/settings.py
+++ starkstore/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'orders.apps.OrdersConfig',
     'users.apps.UsersConfig',
     'services.apps.ServicesConfig',
-    # 'data_collector.apps.DataCollectorConfig'
+    'data_collector.apps.DataCollectorConfig'
 ]
 
 MIDDLEWARE = [
@@ -134,8 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
