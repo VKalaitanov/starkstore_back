ed22477fb7d32af3d4e414a96ddb5148c3d96277
VKalaitanov <kalaytanov93@gmail.com>
2024-10-25 06:47:10 -0700

    add urls

--- starkstore/urls.py
+++ starkstore/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from .yasg import urlpatterns as doc_url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/order/', include('orders.urls')),
     path('api/v1/service/', include('services.urls')),
