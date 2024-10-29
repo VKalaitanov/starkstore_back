5ca14d781efa7f64b312449d2a2c89b567684224
VKalaitanov <kalaytanov93@gmail.com>
2024-10-29 16:56:21 +0200

    dell axes

--- starkstore/settings.py
+++ starkstore/settings.py
@@ -77,7 +77,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'axes.middleware.AxesMiddleware',
+    # 'axes.middleware.AxesMiddleware',
 ]
 
 ROOT_URLCONF = 'starkstore.urls'
