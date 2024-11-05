d7a52bca3d97d01656a50157705a74505aabcf04
VKalaitanov <kalaytanov93@gmail.com>
2024-11-05 18:26:01 +0300

    add new urls

--- starkstore/urls.py
+++ starkstore/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from users.views import confirm_email
 from .yasg import urlpatterns as doc_url
 
 urlpatterns = [
@@ -8,6 +9,7 @@ urlpatterns = [
     path('api/v1/users/', include('users.urls')),
     path('api/v1/order/', include('orders.urls')),
     path('api/v1/service/', include('services.urls')),
+    path('activate/', confirm_email, name='confirm_email')
 ]
 
 urlpatterns += doc_url
--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
-    path('confirm-email/', views.confirm_email, name='confirm_email'),
+    # path('confirm-email/', views.confirm_email, name='confirm_email'),
 ]
