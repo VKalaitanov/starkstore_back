057881021a194674a36306acca07f94418eb68a6
VKalaitanov <kalaytanov93@gmail.com>
2024-11-09 14:16:17 +0300

    add v1 in urls

--- starkstore/urls.py
+++ starkstore/urls.py
@@ -6,9 +6,9 @@ from .yasg import urlpatterns as doc_url
 
 urlpatterns = [
     path('api/admin/', admin.site.urls),
-    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/v1/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/v1/order/', include('orders.urls')),
     path('api/v1/service/', include('services.urls')),
     path('api/v1/user/', include('users.urls')),
