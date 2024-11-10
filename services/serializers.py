27af1dd6e8644dc86de3ea227dfff8e089743e6b
VKalaitanov <kalaytanov93@gmail.com>
2024-11-10 15:31:17 +0300

    add discount_price

--- services/serializers.py
+++ services/serializers.py
@@ -4,6 +4,7 @@ from .models import Service, ServiceOption
 
 class ServiceOptionGetAllSerializer(serializers.ModelSerializer):
     discount_percentage = serializers.SerializerMethodField()  # Для вычисления максимальной скидки
+    discounted_price = serializers.SerializerMethodField()  # Для вычисления цены с учётом скидки
 
     class Meta:
         model = ServiceOption
@@ -11,7 +12,8 @@ class ServiceOptionGetAllSerializer(serializers.ModelSerializer):
             'id',
             'category',
             'price_per_unit',
-            'discount_percentage',  # Выводим максимальную скидку
+            'discount_percentage',
+            'discounted_price',  # Цена с учётом скидки
         ]
 
     def get_discount_percentage(self, obj):
@@ -28,6 +30,25 @@ class ServiceOptionGetAllSerializer(serializers.ModelSerializer):
         # Сравниваем скидку пользователя с общей скидкой на услугу
         return max(user_discount_percentage, obj.discount_percentage)
 
+    def get_discounted_price(self, obj):
+        """
+        Возвращает цену с учётом максимальной скидки.
+        """
+        user = self.context.get('user')  # Получаем пользователя из контекста
+        user_discount_percentage = 0
+
+        if user:
+            # Получаем индивидуальную скидку для пользователя
+            user_discount_percentage = obj.get_user_discount(user)
+
+        # Сравниваем скидку пользователя с общей скидкой на услугу
+        max_discount_percentage = max(user_discount_percentage, obj.discount_percentage)
+
+        # Рассчитываем цену с наибольшей скидкой
+        if max_discount_percentage > 0:
+            return obj.price_per_unit * (1 - max_discount_percentage / 100)
+
+        return obj.price_per_unit
 
 class ServiceGetAllSerializer(serializers.ModelSerializer):
     options = ServiceOptionGetAllSerializer(read_only=True, many=True)
