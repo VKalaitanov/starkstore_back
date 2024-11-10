6f09ba8f144a7e1514d98404c1a0f621ee2878fa
VKalaitanov <kalaytanov93@gmail.com>
2024-11-10 14:57:21 +0300

    add discount_price

--- services/serializers.py
+++ services/serializers.py
@@ -3,21 +3,9 @@ from .models import Service, ServiceOption
 
 
 class ServiceOptionGetAllSerializer(serializers.ModelSerializer):
-    discounted_price = serializers.SerializerMethodField()
-
     class Meta:
         model = ServiceOption
         fields = '__all__'
-
-    def get_discounted_price(self, obj):
-        user = self.context.get('user')
-        discounted_price = obj.get_discounted_price(user=user)
-
-        # Преобразуем объект Money в словарь для сериализации
-        return {
-            "amount": discounted_price.amount,
-            "currency": str(discounted_price.currency)
-        }
 
 
 class ServiceGetAllSerializer(serializers.ModelSerializer):
--- services/views.py
+++ services/views.py
@@ -8,9 +8,3 @@ class ServiceGetAllView(ListAPIView):
     serializer_class = ServiceGetAllSerializer
     queryset = Service.objects.all()
     permission_classes = [IsAuthenticated]
-
-    def get_serializer_context(self):
-        # Передаем пользователя в контекст сериализатора
-        context = super().get_serializer_context()
-        context['user'] = self.request.user
-        return context
