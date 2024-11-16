4ff4081627e668162c28ac12307549feb9f5530a
VKalaitanov <kalaytanov93@gmail.com>
2024-11-16 18:16:25 +0200

    add new urls

--- services/urls.py
+++ services/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ServiceGetAllView
 
+from services.views import ServiceListView, ServiceCategoryListView, ServiceOptionListView
 
 urlpatterns = [
     path('services/', ServiceListView.as_view(), name='service-list'),
     path('services/<int:service_id>/categories/', ServiceCategoryListView.as_view(), name='service-category-list'),
-    path('services/<int:service_id>/categories/<str:category>/', ServiceOptionListView.as_view(), name='service-option-list'),
+    path('services/<int:service_id>/categories/<str:category>/', ServiceOptionListView.as_view(),
+         name='service-option-list'),
 ]
