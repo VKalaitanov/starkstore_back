a682664f426a4bb52e957aafb15b92e152dbf882
VKalaitanov <kalaytanov93@gmail.com>
2024-11-16 11:50:56 +0200

    add new ip by server

--- users/signals.py
+++ users/signals.py
@@ -14,34 +14,39 @@ from users.models import CustomerUser
 
 @receiver(pre_save, sender=CustomerUser)
 def deactivate_user_on_email_change(sender, instance, **kwargs):
-    old_user = CustomerUser.objects.get(id=instance.id)
-    if old_user.email != instance.email:
-        instance.is_active = False
+    # Проверяем, существует ли объект в базе данных
+    if instance.id:  # Если объект имеет id, значит он уже существует
+        try:
+            old_user = CustomerUser.objects.get(id=instance.id)
+            if old_user.email != instance.email:  # Email изменился
+                instance.is_active = False
 
-        # Генерация токена активации и id пользователя
-        token = default_token_generator.make_token(instance)
-        uid = urlsafe_base64_encode(str(instance.id).encode())
+                # Генерация токена активации и id пользователя
+                token = default_token_generator.make_token(instance)
+                uid = urlsafe_base64_encode(str(instance.id).encode())
 
-        protocol = 'http'
-        domain = settings.DOMAIN
-        url = f'activate/{uid}/{token}/'
+                protocol = 'http'
+                domain = settings.DOMAIN
+                url = f'{protocol}://{domain}/activate/{uid}/{token}/'
 
-        subject = f'Account activation on {domain}'
-        message = render_to_string('email/activation.html', {
-            'user': instance,
-            'protocol': protocol,
-            'domain': domain,
-            'url': url,
-        })
+                subject = f'Account activation on {domain}'
+                message = render_to_string('email/activation.html', {
+                    'user': instance,
+                    'protocol': protocol,
+                    'domain': domain,
+                    'url': url,
+                })
 
-        email = EmailMessage(
-            subject,
-            message,
-            settings.DEFAULT_FROM_EMAIL,
-            [instance.email],
-        )
-        email.content_subtype = "html"
-        email.send()
+                email = EmailMessage(
+                    subject,
+                    message,
+                    settings.DEFAULT_FROM_EMAIL,
+                    [instance.email],
+                )
+                email.content_subtype = "html"
+                email.send()
+        except CustomerUser.DoesNotExist:
+            pass  # Если объект не найден, ничего не делаем
 
 # @receiver(post_migrate)
 # def create_managers_group(sender, **kwargs):
