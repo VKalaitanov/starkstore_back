7d36732f328deb52c15f5b9b3aabff67049565bb
VKalaitanov <kalaytanov93@gmail.com>
2024-12-01 16:31:00 +0300

    del interval in service

--- services/admin.py
+++ services/admin.py
@@ -11,7 +11,7 @@ admin.site.register(PointsServiceOption)
 
 class ServiceOptionAdmin(admin.ModelAdmin):
     form = ServiceOptionAdminForm
-    search_fields = ['service__name', 'name', 'required_field', 'points', 'use_interval', 'interval']
+    search_fields = ['service__name', 'name', 'required_field', 'points']
 
 
 admin.site.register(ServiceOption, ServiceOptionAdmin)
