f046c3f46cf220b340cb6dc435219530131848be
VKalaitanov <kalaytanov93@gmail.com>
2025-01-11 17:51:01 +0300

    add PopularServiceOption

--- services/urls.py
+++ services/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('services/<int:service_id>/categories/', ServiceCategoryListView.as_view(), name='service-category-list'),
     path('services/<int:service_id>/categories/<str:category>/', ServiceOptionListView.as_view(),
          name='service-option-list'),
-    path('popular-services/<int:popular_service_id>/', ServiceOptionListView.as_view(), name='popular-service-detail'),
+    path('popular-services/', ServiceOptionListView.as_view(), name='popular-service-detail'),
     path('calculate-price/', CalculateOrderPriceView.as_view(), name='calculate-price'),
 ]
