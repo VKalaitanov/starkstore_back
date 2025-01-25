963a756629f670f95d56dacb32c71cb23170165b
VKalaitanov <kalaytanov93@gmail.com>
2025-01-25 14:13:39 +0300

    add date analytic

--- data_collector/admin.py
+++ data_collector/admin.py
@@ -6,47 +6,72 @@ from .models import DailyOrderAnalytics, AllTimeOrderAnalytics
 
 @admin.register(DailyOrderAnalytics)
 class DailyOrderAnalyticsAdmin(admin.ModelAdmin):
-    fields = ['date', 'total_orders', 'completed_orders', 'total_revenue',
-              'get_table_history_orders']
-    readonly_fields = fields
+    list_display = ['date', 'total_orders', 'completed_orders', 'total_revenue']
+    list_filter = ['date']  # Фильтр по дате
+    readonly_fields = ['date', 'total_orders', 'completed_orders', 'total_revenue', 'get_table_history_orders']
+    actions = ['download_selected_statistics']  # Кастомное действие
 
     @admin.display(description='Таблица завершенных заказов за 1 день')
     def get_table_history_orders(self, obj: DailyOrderAnalytics):
         table_html = """
-                        <table style="width:100%; border: 1px solid black; border-collapse: collapse;">
-                            <thead>
-                                <tr>
-                                    <th style="border: 1px solid black; padding: 5px;">Сервис</th>
-                                    <th style="border: 1px solid black; padding: 5px;">Категория</th>
-                                    <th style="border: 1px solid black; padding: 5px;">Пользователь</th>
-                                    <th style="border: 1px solid black; padding: 5px;">Дата завершения заказа</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                        """
+            <table style="width:100%; border: 1px solid black; border-collapse: collapse;">
+                <thead>
+                    <tr>
+                        <th style="border: 1px solid black; padding: 5px;">Сервис</th>
+                        <th style="border: 1px solid black; padding: 5px;">Категория</th>
+                        <th style="border: 1px solid black; padding: 5px;">Пользователь</th>
+                        <th style="border: 1px solid black; padding: 5px;">Дата завершения заказа</th>
+                    </tr>
+                </thead>
+                <tbody>
+        """
         history_orders = zip(obj.info_completed_orders['service'], obj.info_completed_orders['option'],
                              obj.info_completed_orders['user'], obj.info_completed_orders['completed'])
 
-        if history_orders:
-            for service, option, user, completed in history_orders:
-                table_html += f"""
-                        <tr>
-                            <td style="border: 1px solid black; padding: 5px;">{service}</td>
-                            <td style="border: 1px solid black; padding: 5px;">{option}</td>
-                            <td style="border: 1px solid black; padding: 5px;">{user}</td>
-                            <td style="border: 1px solid black; padding: 5px;">{completed}</td>
-                        </tr>
-                        """
-        else:
+        for service, option, user, completed in history_orders:
+            table_html += f"""
+                <tr>
+                    <td style="border: 1px solid black; padding: 5px;">{service}</td>
+                    <td style="border: 1px solid black; padding: 5px;">{option}</td>
+                    <td style="border: 1px solid black; padding: 5px;">{user}</td>
+                    <td style="border: 1px solid black; padding: 5px;">{completed}</td>
+                </tr>
+            """
+
+        if not history_orders:
             table_html += """
-                        <tr>
-                            <td colspan="3" style="border: 1px solid black; padding: 5px; text-align: center;">История баланса отсутствует</td>
-                        </tr>
-                        """
+                <tr>
+                    <td colspan="4" style="border: 1px solid black; padding: 5px; text-align: center;">
+                        История отсутствует
+                    </td>
+                </tr>
+            """
 
         table_html += "</tbody></table>"
-
         return format_html(table_html)
+
+    def download_selected_statistics(self, request, queryset):
+        """
+        Экспорт данных за выбранные дни в CSV.
+        """
+        import csv
+        from django.http import HttpResponse
+
+        # Настройка ответа с заголовками для скачивания
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="statistics.csv"'
+
+        writer = csv.writer(response)
+        # Заголовки
+        writer.writerow(['Дата', 'Общее количество заказов', 'Завершённые заказы', 'Общий доход'])
+
+        # Заполнение данными из выбранных объектов
+        for obj in queryset:
+            writer.writerow([obj.date, obj.total_orders, obj.completed_orders, obj.total_revenue])
+
+        return response
+
+    download_selected_statistics.short_description = 'Скачать статистику по выбранным дням'
 
     def has_add_permission(self, request):
         return False
